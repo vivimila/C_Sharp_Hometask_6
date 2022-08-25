1a21ccaab56858a0d8e9804dc2f7537924c6b12f
vivimila <violamilashevskaya@yandex.by>
2022-08-25 17:54:26 +0300

    Task 43 is solved

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -4,3 +4,25 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+// int SearchX = 0; 
+// int SearchY = 0; 
+
+if ((k1 == k2) && (b1 == b2))
+    Console.Write("Прямые совпадают");
+else if (k1 == k2)
+    Console.Write("Прямые параллельны");
+
+else
+{
+    double SearchX = (b2 - b1) / (k1 - k2);
+    double SearchY = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+    Console.WriteLine($"Координаты точки пересечения прямой: [{SearchX};{SearchY}]");
+}
